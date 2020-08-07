fd95a22c13ee58e6abf50850ba26c47b24486d6a
Orhan Gulec <orhangulec@Orhan-MacBook-Pro.local>
2020-08-07 12:26:44 +0300

    08 07 2nd

---
+++ src/components/CityNature/Content.vue
@@ -0,0 +1,85 @@
+<template>
+    
+         
+                        <div class="city">
+                            
+                            <div class="flex align-ver align-hor city-photo">
+                                <img src="~@/assets/images/city1.jpg">
+                                <img src="~@/assets/images/city2.jpg">
+                                <img src="~@/assets/images/city3.jpg">
+
+                            </div>
+
+                            <div class="flex align-ver align-hor city-photo-inner">
+                                <img src="~@/assets/images/city4.jpg">
+                                <img src="~@/assets/images/city5.jpg">
+                                <img src="~@/assets/images/city6.jpg">
+
+                            </div>
+                            <div class="flex align-ver align-hor city-photo">
+                                <img src="~@/assets/images/city7.jpg">
+                                <img src="~@/assets/images/city8.jpg">
+                                <img src="~@/assets/images/city9.jpg">
+
+                            </div>
+                            <div class="flex align-ver align-hor city-photo-vertical">
+                                <img src="~@/assets/images/city10.jpg">
+                                <img src="~@/assets/images/city11.jpg">
+                                <img src="~@/assets/images/city12.jpg">
+
+                            </div>
+                            <div class="flex align-ver align-hor city-photo">
+                                <img src="~@/assets/images/city13.jpg">
+                                <img src="~@/assets/images/city14.jpg">
+                                <img src="~@/assets/images/city15.jpg">
+
+                            </div>
+                            <div class="flex align-ver align-hor city-photo-inner">
+                                <img src="~@/assets/images/city16.jpg">
+                                <img src="~@/assets/images/city17.jpg">
+                                <img src="~@/assets/images/city18.jpg">
+
+                            </div>
+                            <div class="flex align-ver align-hor city-photo">
+                                <img src="~@/assets/images/city19.jpg">
+                                <img src="~@/assets/images/city20.jpg">
+                                <img src="~@/assets/images/city21.jpg">
+
+                            </div>
+
+                        </div>
+                
+        
+    
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.city{
+    background-color: black;
+    
+}
+
+
+.city-photo img{
+    height: 300px;
+    border: 4px solid white;
+    margin: 20px 5px;
+}
+.city-photo-inner img{
+    height: 300px;
+    border: 4px solid white;
+    margin: 0 5px;
+}
+.city-photo-vertical img{
+    height: 675px;
+    border: 4px solid white;
+    margin:  5px;
+}    
+</style>
+
--- src/views/Portfolio/Citynature.vue
+++ src/views/Portfolio/Citynature.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <app-header></app-header>
+        <app-content></app-content>
     </div>
 </template>
 
 <script>
 import appHeader from "../../components/CityNature/Header"
+import appContent from "../../components/CityNature/Content"
 export default {
     components:{
         appHeader,
+        appContent
     }
     
 }
